0f7305482d897d03ba14e2f885770a2aed74c96a
AnastasiiaRadasheva <eha20082@gmail.com>
2025-02-03 16:40:55 +0200

    methobcall

---
+++ IFElseFootSIze/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace IFElseFootSIze
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Sisesta jalanumber:");
+        int jalanumber = int.Parse(Console.ReadLine());
+
+        if (jalanumber >= 30 && jalanumber <= 33)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Jalanumber on vahemikus 30 kuni 33.");
+        }
+        else if (jalanumber >= 34 && jalanumber <= 38)
+        {
+            Console.ResetColor();
+            Console.WriteLine("Jalanumber on vahemikus 34 kuni 38.");
+        }
+        else if (jalanumber >= 39 && jalanumber <= 44)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Jalanumber on vahemikus 39 kuni 44.");
+            Console.ResetColor();
+        }
+        else if (jalanumber >= 45 && jalanumber <= 48)
+        {
+            Console.Beep();
+            Console.WriteLine("Jalanumber on vahemikus 45 kuni 48.");
+        }
+        else
+        {
+            Console.WriteLine("Jalanumber ei kuulu määratud vahemikku.");
+        }
+
+        Console.ResetColor();
+    }
+}
---
+++ IfElseFootSIze1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace IfElseFootSIze1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write size");
+            int size = int.Parse(Console.ReadLine());
+            if (size >= 30 && size <= 33)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Small size");
+            }
+            else if (size >= 34 && size <= 38)
+            {
+                Console.ResetColor();
+                Console.WriteLine("Medium size");
+            }
+            else if (size >= 39 && size <= 44)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Large size");
+            }
+            else if (size >= 45 && size <= 48)
+            {
+                Console.Beep();
+                Console.WriteLine("Jalanumber on vahemikus 45 kuni 48.");
+            }
+            else
+            {
+                Console.WriteLine("Jalanumber ei kuulu määratud vahemikku.");
+            }
+        }
+    }
+}
